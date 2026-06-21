503d9e1c33a3b8351ad6dfc37c432d9dda72a5f1
yasar-my <yasarm8675@gmail.com>
2026-06-21 23:34:07 +0530

    home page img-update

--- Redesigned-Frontend/feast-fete/src/pages/Home.jsx
+++ Redesigned-Frontend/feast-fete/src/pages/Home.jsx
@@ -323,7 +323,7 @@ const Home = () => {
                 {[
                     {
                         title: "Wedding Catering",
-                        img: {WeddingImg},
+                        img: "../assets/Wedding.jpg",
                     },
                     {
                         title: "Corporate Events",
